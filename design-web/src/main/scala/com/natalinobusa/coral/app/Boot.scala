ef75e64b43485ead4eabe44e019d1aa94f81d059
Natalino Busa <natalino.busa@gmail.com>
2015-01-20 00:52:28 +0100

    RESTfull collection of resources

--- design-web/src/main/scala/com/natalinobusa/coral/app/Boot.scala
+++ design-web/src/main/scala/com/natalinobusa/coral/app/Boot.scala
@@ -9,8 +9,11 @@ object Boot extends App {
 
   // create and start our service actor
   val service = system.actorOf(Props[ApiServiceActor], "api")
-  
-  // start a new HTTP server with our service actor as the handler
-  IO(Http) ! Http.Bind(service, "0.0.0.0", port = 8888)
 
+  // fetch configuration from resources/application.config
+  val interface = system.settings.config getString "service.interface"
+  val port      = system.settings.config getInt    "service.port"
+
+  // start a new HTTP server with our service actor as the handler
+  IO(Http) ! Http.Bind(service, interface, port)
 }
